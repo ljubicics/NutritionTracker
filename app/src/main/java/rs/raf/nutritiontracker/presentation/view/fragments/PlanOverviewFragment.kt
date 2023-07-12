b5a1c1b6f098e66a697f962b7fd36128665625d4
ljubicics <strahinja.ljubicic@gmail.com>
2023-07-13 00:27:16 +0200

    Finished plan sending functionality

--- app/src/main/java/rs/raf/nutritiontracker/presentation/view/fragments/PlanOverviewFragment.kt
+++ app/src/main/java/rs/raf/nutritiontracker/presentation/view/fragments/PlanOverviewFragment.kt
@@ -1,14 +1,17 @@
 package rs.raf.nutritiontracker.presentation.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import rs.raf.nutritiontracker.R
 import rs.raf.nutritiontracker.data.models.DayInTheWeek
+import rs.raf.nutritiontracker.data.models.MealForPlan
 import rs.raf.nutritiontracker.databinding.FragmentFilterAreaBinding
 import rs.raf.nutritiontracker.databinding.FragmentPlanOverviewBinding
 import rs.raf.nutritiontracker.presentation.contract.PickMealsContract
@@ -133,7 +136,99 @@ class PlanOverviewFragment : Fragment(R.layout.fragment_plan_overview) {
             transaction.add(R.id.container, PlanForDayFragment(DayInTheWeek.SUNDAY)).addToBackStack(null)
             transaction.commit()
         }
+        binding.sendPlanButton.setOnClickListener {
+            var mealsMonday: List<MealForPlan> = listOf()
+            var mealsTuesday: List<MealForPlan> = listOf()
+            var mealsWednesday: List<MealForPlan> = listOf()
+            var mealsThursday: List<MealForPlan> = listOf()
+            var mealsFriday: List<MealForPlan> = listOf()
+            var mealsSaturday: List<MealForPlan> = listOf()
+            var mealsSunday: List<MealForPlan> = listOf()
+
+            if(pickMealsViewModel.mondayState.value != null) {
+                mealsMonday = pickMealsViewModel.mondayState.value!!
+            }
+            if(pickMealsViewModel.tuesdayState.value != null) {
+                mealsTuesday = pickMealsViewModel.tuesdayState.value!!
+            }
+            if(pickMealsViewModel.wednesdayState.value != null) {
+                mealsWednesday = pickMealsViewModel.wednesdayState.value!!
+            }
+            if(pickMealsViewModel.thursdayState.value != null) {
+                mealsThursday = pickMealsViewModel.thursdayState.value!!
+            }
+            if(pickMealsViewModel.fridayState.value != null) {
+                mealsFriday = pickMealsViewModel.fridayState.value!!
+            }
+            if(pickMealsViewModel.saturdayState.value != null) {
+                mealsSaturday = pickMealsViewModel.saturdayState.value!!
+            }
+            if(pickMealsViewModel.sundayState.value != null) {
+                mealsSunday = pickMealsViewModel.sundayState.value!!
+            }
+            var message = "Your meal plan: \n\n"
+            message += "Monday:\n"
+            for (meal in mealsMonday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "Tuesday:\n"
+            for (meal in mealsTuesday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "Wednesday:\n"
+            for (meal in mealsWednesday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "Thursday:\n"
+            for (meal in mealsThursday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "Friday:\n"
+            for(meal in mealsFriday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "Saturday:\n"
+            for (meal in mealsSaturday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "Sunday:\n"
+            for(meal in mealsSunday) {
+                message += "Name: ${meal.strMeal}\n"
+                message += "Meal Type: ${meal.mealType}\n"
+                message += "--------------------\n"
+            }
+            message += "\nSincerely,\nYour NutritionTracker team"
+            sendEmail(binding.planET.text.toString(), "Your meal plan", message)
+        }
     }
+
+    private fun sendEmail(emailAddress: String, subject: String, message: String) {
+        val appLink = "myapp://open/"
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emailAddress))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+
+        if (intent.resolveActivity(context!!.packageManager) != null) {
+            startActivity(Intent.createChooser(intent, "Send Email"))
+        } else {
+            Toast.makeText(context, "No email client found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
